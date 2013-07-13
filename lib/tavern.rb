c9bf605555972e5d99d20c362558d9dea5c75748
ignaciocontreras <ignacio@ignacio.cat>
2013-07-13 08:57:17 +0200

    Add first Task class implementation

--- lib/tavern.rb
+++ lib/tavern.rb
@@ -1,5 +1,30 @@
 require "tavern/version"
 
 module Tavern
-  # Your code goes here...
+  
+  class Task
+    attr_accessor :status, :priority, :text, :tags
+    STATUS = [:todo, :done]
+    PRIORITY = [:low, :normal, :high]
+
+    self.class_eval do
+      STATUS.each do |s|
+        define_method("#{s}?") { self.status == s }
+        define_method("#{s}!") { self.status = s }
+      end
+      PRIORITY.each do |p|
+        define_method("#{p}?") { self.priority == p }
+        define_method("#{p}!") { self.priority = p }
+      end
+    end
+
+    def initialize(text, *tags)
+      self.status = :todo
+      self.priority = :normal
+      self.text = text
+      self.tags = tags || []
+    end
+
+  end
+
 end
